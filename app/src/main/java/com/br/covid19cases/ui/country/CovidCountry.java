d2e41ad21dbca4ce48fb12a6733ad5379a03e329
abelaraujo <abel2901@gmail.com>
2021-04-12 11:56:18 -0300

    create repository

---
+++ app/src/main/java/com/br/covid19cases/ui/country/CovidCountry.java
@@ -0,0 +1,124 @@
+package com.br.covid19cases.ui.country;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class CovidCountry implements Parcelable {
+
+    String mCovidCountry, mCases, mTodayCases, mDeaths, mTodayDeaths, mRecovered, mActive, mCritical;
+
+    public CovidCountry(String mCovidCountry, String mCases, String mTodayCases, String mDeaths, String mTodayDeaths, String mRecovered, String mActive, String mCritical) {
+        this.mCovidCountry = mCovidCountry;
+        this.mCases = mCases;
+        this.mTodayCases = mTodayCases;
+        this.mDeaths = mDeaths;
+        this.mTodayDeaths = mTodayDeaths;
+        this.mRecovered = mRecovered;
+        this.mActive = mActive;
+        this.mCritical = mCritical;
+    }
+
+    public void setmCovidCountry(String mCovidCountry) {
+        this.mCovidCountry = mCovidCountry;
+    }
+
+    public void setmCases(String mCases) {
+        this.mCases = mCases;
+    }
+
+    public void setmTodayCases(String mTodayCases) {
+        this.mTodayCases = mTodayCases;
+    }
+
+    public void setmDeaths(String mDeaths) {
+        this.mDeaths = mDeaths;
+    }
+
+    public void setmTodayDeaths(String mTodayDeaths) {
+        this.mTodayDeaths = mTodayDeaths;
+    }
+
+    public void setmRecovered(String mRecovered) {
+        this.mRecovered = mRecovered;
+    }
+
+    public void setmActive(String mActive) {
+        this.mActive = mActive;
+    }
+
+    public void setmCritical(String mCritical) {
+        this.mCritical = mCritical;
+    }
+
+    public String getmCovidCountry() {
+        return mCovidCountry;
+    }
+
+    public String getmCases() {
+        return mCases;
+    }
+
+    public String getmTodayCases() {
+        return mTodayCases;
+    }
+
+    public String getmDeaths() {
+        return mDeaths;
+    }
+
+    public String getmTodayDeaths() {
+        return mTodayDeaths;
+    }
+
+    public String getmRecovered() {
+        return mRecovered;
+    }
+
+    public String getmActive() {
+        return mActive;
+    }
+
+    public String getmCritical() {
+        return mCritical;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mCovidCountry);
+        dest.writeString(this.mCases);
+        dest.writeString(this.mTodayCases);
+        dest.writeString(this.mDeaths);
+        dest.writeString(this.mTodayDeaths);
+        dest.writeString(this.mRecovered);
+        dest.writeString(this.mActive);
+        dest.writeString(this.mCritical);
+    }
+
+    protected CovidCountry(Parcel in) {
+        this.mCovidCountry = in.readString();
+        this.mCases = in.readString();
+        this.mTodayCases = in.readString();
+        this.mDeaths = in.readString();
+        this.mTodayDeaths = in.readString();
+        this.mRecovered = in.readString();
+        this.mActive = in.readString();
+        this.mCritical = in.readString();
+    }
+
+    public static final Creator<CovidCountry> CREATOR = new Creator<CovidCountry>() {
+        @Override
+        public CovidCountry createFromParcel(Parcel in) {
+            return new CovidCountry(in);
+        }
+
+        @Override
+        public CovidCountry[] newArray(int size) {
+            return new CovidCountry[size];
+        }
+    };
+}
---
+++ app/src/main/java/com/br/covid19cases/ui/country/CovidCountryDetail.java
@@ -0,0 +1,45 @@
+package com.br.covid19cases.ui.country;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.br.covid19cases.R;
+
+public class CovidCountryDetail extends AppCompatActivity {
+
+    TextView tvDetailCountryName, tvDetailTotalCases, tvDetailTodayCases, tvDetailTotalDeaths,
+            tvDetailTodayDeaths, tvDetailTotalRecovered, tvDetailTotalActive, tvDetailTotalCritical;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covid_country_detail);
+
+        ///call view
+        tvDetailCountryName = findViewById(R.id.tvDetailContryName);
+        tvDetailTotalCases = findViewById(R.id.tvDetailTotalCases);
+        tvDetailTodayCases = findViewById(R.id.tvDetailTodayCases);
+        tvDetailTotalDeaths = findViewById(R.id.tvDetailTotalDeaths);
+        tvDetailTodayDeaths = findViewById(R.id.tvDetailTodayDeaths);
+        tvDetailTotalRecovered = findViewById(R.id.tvDetailTotalRecovered);
+        tvDetailTotalActive = findViewById(R.id.tvDetailTotalActive);
+        tvDetailTotalCritical = findViewById(R.id.tvDetailTotalCritical);
+
+
+        // call covid country
+
+        CovidCountry covidCountry = getIntent().getParcelableExtra("EXTRA_COVID");
+
+        // setText
+        tvDetailCountryName.setText(covidCountry.getmCovidCountry());
+        tvDetailTotalCases.setText(covidCountry.getmCases());
+        tvDetailTodayCases.setText(covidCountry.getmTodayCases());
+        tvDetailTotalDeaths.setText(covidCountry.getmDeaths());
+        tvDetailTodayDeaths.setText(covidCountry.getmTodayDeaths());
+        tvDetailTotalRecovered.setText(covidCountry.getmRecovered());
+        tvDetailTotalActive.setText(covidCountry.getmActive());
+        tvDetailTotalCritical.setText(covidCountry.getmCritical());
+    }
+}
